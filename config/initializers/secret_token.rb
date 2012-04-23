ccd7464b2ee4e29872ae1190fcdc21af5399c259
Nawal Behih <nabehih@gmail.com>
2012-04-23 20:17:25 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExamPra::Application.config.secret_token = '0d1b76a6b70ad7341e762e4754053e3fedc70930a5450261ba9302ef1a044de6160ad240b733dfcd2f401927fce6d8440bebcc897bf8810422ad07a2662b2e80'
